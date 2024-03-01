8284b861ea85549bfe503df100096d48d862dd16
Conor Chitolie <conor.chitolie@fujitsu.com>
2024-03-01 15:32:33 +0000

    Added checkboxes for different input parameters

--- src/main/java/com/codingchallenge/demo/controller/HomeController.java
+++ src/main/java/com/codingchallenge/demo/controller/HomeController.java
@@ -17,9 +17,15 @@ public class HomeController {
     }
 
     @PostMapping("/generate")
-    public String generatePassword(@RequestParam int passwordLength, Model model) {
+    public String generatePassword(
+        @RequestParam int passwordLength,
+        @RequestParam(defaultValue = "false") boolean numericalCharacters,
+        @RequestParam(defaultValue = "false") boolean specialCharacters,
+        @RequestParam(defaultValue = "false") boolean upperCaseCharacters,
+        @RequestParam(defaultValue = "false") boolean lowerCaseCharacters, Model model) {
+
         PasswordGenerator passwordGenerator = new PasswordGenerator();
-        String password = passwordGenerator.generatePassword(passwordLength);
+        String password = passwordGenerator.generatePassword(passwordLength, numericalCharacters, specialCharacters, upperCaseCharacters, lowerCaseCharacters);
         model.addAttribute("password", password);
         return "passwordResult";
     }
--- src/main/java/com/codingchallenge/demo/model/PasswordGenerator.java
+++ src/main/java/com/codingchallenge/demo/model/PasswordGenerator.java
@@ -1,10 +1,28 @@
 package com.codingchallenge.demo.model;
 
 public class PasswordGenerator {
-    public String generatePassword(int length) {
-        // Implement your password generation logic here
-        // This can be a simple algorithm or using a library like Apache Commons Lang
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    public String generatePassword(int length, boolean numericalCharacters, boolean specialCharacters, boolean upperCaseCharacters, boolean lowerCaseCharacters) {
+        // Setup character set based on input booleans
+        StringBuilder characters = new StringBuilder();
+
+        if (numericalCharacters == true) {
+            characters.append("0123456789");
+        }
+        if (specialCharacters == true) {
+            characters.append("!@#$%^&*()-_=+[]{}|;:'\",.<>/?");
+        }
+        if (upperCaseCharacters == true) {
+            characters.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        }
+        if (specialCharacters == true) {
+            characters.append("abcdefghijklmnopqrstuvwxyz");
+        }
+
+        if (characters.length() == 0 || (!numericalCharacters & !specialCharacters & !upperCaseCharacters & !lowerCaseCharacters)) {
+            // If no character set is selected, use the default set
+            characters.append("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+        }
+        
         StringBuilder password = new StringBuilder();
 
         for (int i = 0; i < length; i++) {
